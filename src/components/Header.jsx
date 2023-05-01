0ba99b02d04453f8ae2e2fa72ca991142c08739b
Mitchel Ogunniyi <isokandone@gmail.com>
2023-05-01 18:58:14 +0100

    Header.js file edited

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Resume and Cover Letter Generator</h1>
+      <img src="path/to/logo.png" alt="Logo" />
+    </header>
+  );
+}
+
+export default Header;
