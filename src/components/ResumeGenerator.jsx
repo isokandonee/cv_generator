827fb305de99cfb56c9e09753c227233c516fe7f
Mitchel Ogunniyi <isokandone@gmail.com>
2023-05-01 19:22:02 +0100

    ResumeGenerator.jsx component has been created

---
+++ src/components/ResumeGenerator.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import ResumeForm from './ResumeForm';
+import ResumePreview from './ResumePreview';
+
+function ResumeGenerator() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    education: '',
+    workExperience: '',
+    skills: '',
+  });
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    // add code here to generate pdf or word document
+  };
+
+  const handleFormReset = () => {
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      education: '',
+      workExperience: '',
+      skills: '',
+    });
+  };
+
+  return (
+    <div>
+      <h1>Resume Generator</h1>
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <ResumeForm formData={formData} setFormData={setFormData} />
+          <button type="submit" onClick={handleFormSubmit}>Generate</button>
+          <button type="reset" onClick={handleFormReset}>Reset</button>
+        </div>
+        <div style={{ flex: 1 }}>
+          <ResumePreview formData={formData} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ResumeGenerator;
